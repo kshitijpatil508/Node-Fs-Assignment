66940162cb1fde3d2e987a638313a43e4f94fb46
Kshitij Patil <46442320+kshitijpatil508@users.noreply.github.com>
2023-04-18 17:16:41 +0530

    Update sorted.js

--- sorted.js
+++ sorted.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 
+// to check if file paths are given
 if (process.argv.length < 4) {
-  console.error("Please provide input and output file paths.");
+  console.error("Please give input and output file paths.");
   process.exit(1);
 }
 
+// take I/O from command line
 const inputFile = process.argv[2];
 const outputFile = process.argv[3];
 
+// check for input file
 if (!fs.existsSync(inputFile)) {
   console.error(`File not found: ${inputFile}`);
   process.exit(1);
@@ -15,14 +18,18 @@ if (!fs.existsSync(inputFile)) {
 
 const fileContents = fs.readFileSync(inputFile, "utf-8");
 
+// convert lines into array
 const lines = fileContents.split("\n");
 
+// removing empty lines and comments
 const filteredLines = lines.filter((line) => {
   return line.trim() !== "" && !line.startsWith("#");
 });
 
+// sorting
 const sortedLines = filteredLines.sort();
 
+// writing to outpit file
 try {
   fs.writeFileSync(outputFile, sortedLines.join("\n"));
   console.log(`Sorted lines written to ${outputFile}`);
